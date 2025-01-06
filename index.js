acb585617e83384a85eebc9df84b593facbdca1b
Matheous Salandanan <arylmatheous22@gmail.com>
2025-01-06 10:59:56 +0800

    feat: :zap: FIX PORT BUG

--- index.js
+++ index.js
@@ -87,6 +87,6 @@ app.use("/AddOrder", submitMenuSystem);
 const PORT = process.env.PORT || 3000;
 
 server .listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 }); 
 
